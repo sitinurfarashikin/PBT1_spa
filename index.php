95128a2da9743fa776a00763bfb2f185db8ecb3c
sitinurfarashikin <sitinurfarashikin@gmail.com>
2021-12-23 10:48:23 +0800

    new

--- index.php
+++ index.php
@@ -12,59 +12,99 @@
     <h1>Welcome To Spa And Wellness</h1>
     <h4>FOUR SEASONS PLACE, 145 JALAN AMPANG, 50450 KUALA LUMPUR, MALAYSIA</h4>
 
-    <form action="function.php" method="post">
+    <h4>Hour of service : Spa Treatments</h4>
+    <h5>From 11:00 AM To 8:30 PM</h5>
 
-        <label>First Name:</label>
-        <input type="text" for="fname" name="fname" id="fname"><br><br>
+    <h4>List of service:</h4>
+    <table>
 
-        <label>Last Name:</label>
-        <input type="text" for="lname" name="lname" id="lname"><br><br>
+        <tr>
 
-        <label>Age:</label>
-        <input type="number" for="age" name="age" id="age"><br><br>
+            <th>SPA & SALON SERVICES</th>
 
-        <label>Gender:</label>
-        <input type="radio" for="Jantina" name="jantina" value="female" id="female">
-        <label>Female</label>
-        <input type="radio" for="Jantina" name="jantina" value="male" id="male">
-        <label>Male</label><br><br>
+            <th>Price per services</th>
 
-        <label>Address:</label>
-        <textarea id="address" name="address" rows="4" cols="40"></textarea><br><br>
+        </tr>
 
-        <label>Email:</label>
-        <input type="text" for="email" name="email" id="email"><br><br>
+        <tr>
 
-        <label>Number telephone:</label>
-        <input type="text" for="notelefon" name="notelefon" id="notelefon"><br><br>
+            <td>MASSAGES</td>
 
-        <label>Spa & Salon Services:</label>
-        <select id="services" name="services">
-            <option value=""></option>
-            <option value="massage">MASSAGES</option>
-            <option value="bodyrenewal">BODY RENEWAL</option>
-            <option value="facials">FACIALS</option>
-            <option value="skincare">SKINCARE</option>
-        </select><br><br>
+            <td>RM659</td>
 
-        <label>Retails:</label>
-        <input type="checkbox" for="skills" name="micellar" value="micellar" id="micellar">
-        <label>CLEANSING MICELLAR WATER 200ML</label>
+        </tr>
 
-        <input type="checkbox" for="skills" name="cleansing" value="cleansing" id="cleansing">
-        <label>DELICATE CLEANSING MILK 250ML</label>
 
-        <input type="checkbox" for="skills" name="moisturizing" value="moisturizing" id="moisturizing">
-        <label>PH MOISTURIZING BOOSTER 30ML</label>
+        <h4>Additional service</h4>
 
-        <input type="checkbox" for="skills" name="eyecream" value="eyecream" id="eyecream">
-        <label>SOOTHING EYE CREAM 15ML</label>
+        <table>
 
-        <input type="checkbox" for="skills" name="spf" value="spf" id="spf">
-        <label>PROTECTIVE CREAM SPF 50+ "BLUE LIGHT TECHNOLOGY"</label> <br><br>
+            <tr>
 
-        <button type="submit">SUBMIT</button>
-    </form>
+                <th colspan="2">Optional</th>
+
+            </tr>
+
+            <tr>
+
+                <td>RETAIL</td>
+
+                <td>RM60</td>
+
+            </tr>
+
+        </table>
+
+        <br><br><br>
+
+        <h4>Please insert form below to booking:</h4>
+
+        <form action="function.php" method="post">
+
+            <table>
+
+                <tr>
+
+                    <th>Name</th>
+
+                    <td><input type="text" class="form-control" name="name"></td>
+
+                </tr>
+
+                <tr>
+
+                    <th>Room type</th>
+
+                    <td>
+
+                        <select name="room" id="room" class="form-control">
+
+                            <option value=""></option>
+
+                            <option value="Deep Relaxing Marma">Deep Relaxing Marma</option>
+
+                            <option value="Tradisional Balinese">Tradisional Balinese</option>
+
+                            <option value="Tender Touch">Tender Touch</option>
+
+                            <option value="Ayuveda Foot Massage">Ayuveda Foot Massage</option>
+
+                        </select>
+
+                    </td>
+
+                </tr>
+
+                
+
+            </table>
+
+            <br>
+
+            <button id="btn" name="submit" type="submit" class="btn btn-primary">Submit</button>
+
+        </form>
+
 </body>
 
 </html>
